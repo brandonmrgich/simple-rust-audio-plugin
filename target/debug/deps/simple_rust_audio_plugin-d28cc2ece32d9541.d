71b5dc51493da56eb8c82c04b79bc0d721341644
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2022-11-14 22:54:48 -0500

    innit cargo

---
+++ target/debug/deps/simple_rust_audio_plugin-d28cc2ece32d9541.d
@@ -0,0 +1,5 @@
+/Users/brandon/Documents/GitHubProjects/simple-rust-audio-plugin/target/debug/deps/simple_rust_audio_plugin-d28cc2ece32d9541: src/main.rs
+
+/Users/brandon/Documents/GitHubProjects/simple-rust-audio-plugin/target/debug/deps/simple_rust_audio_plugin-d28cc2ece32d9541.d: src/main.rs
+
+src/main.rs:
